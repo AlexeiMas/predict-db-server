bf75d3df21e525fd0ec2ad102e7fedfe7f133831
albertbezman <a@DESKTOP-5EM4NNQ.localdomain>
2021-07-27 21:11:28 +0100

    change emails 3

--- controllers/admin/users/edit.controller.js
+++ controllers/admin/users/edit.controller.js
@@ -40,6 +40,8 @@ module.exports = async (req, res) => {
           <p>Your account for PredictDb is now ready to go.</p>
           <br>
           <p>You can login into <a href='https://pdb.imagentherapeutics.com'>PredictDb</a> now. ��</p>
+          <br/>
+          <p class="x_MsoNormal" style="text-align:justify"><a href="https://imagentherapeutics.com/" target="_blank" rel="noopener noreferrer" data-auth="NotApplicable" data-linkindex="0"><span lang="EN-GB" style="color:windowtext; text-decoration:none"><img naturalheight="0" naturalwidth="0" src="https://imagentherapeutics.com/wp-content/uploads/2020/06/Logo-New-Rect-Smoothed-1.png" border="0" width="253" height="121" id="x_Picture_x0020_1" style="width: 2.6333in; height: 1.2583in;" ></span></a><span lang="EN-GB" style=""></span></p>
         `,
       });
     }
