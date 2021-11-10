f381f33b99f1968b8e72fbc8b7ec81f66b1dd70f
AlekseyG <agolovin@mentalstack.com>
2021-11-10 17:25:39 +0300

    remove extra fields from the export spreadsheet in sheet "Patient Treatment History"

--- services/export/sheets/history.service.js
+++ services/export/sheets/history.service.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-nested-ternary */
-const moment = require('moment');
 const {
   sheetOptions, alignmentStyle, greyHeaderStyle, blueHeaderStyle, setSheetBasicLayout, fillHeaderFields, fillValues,
 } = require('../helpers');
@@ -11,8 +9,6 @@ const FIELDS = [
   'Pre/Post Collection',
   'Regime',
   'Treatments',
-  'Date Started',
-  'Date of Last Treatment',
   'Dose (mg/day or mg/kg)',
   'Treatment Duration (Months)',
   'Best Response (RECIST)',
@@ -30,33 +26,18 @@ const prepareValues = (data) => data.reduce((acc, row) => {
 
   if (!history) return [...acc, ...modelInfo];
 
-  const historyRows = history.map((item) => {
-    const DATE_STARTED = item['Date Started'] && item['Date Started'].length > 0
-      ? item['Date Started'].length === 10
-        ? moment(item['Date Started']).format('YYYY-MMMM-DD')
-        : item['Date Started']
-      : '';
-
-    const DATE_OF_LAST_TREATMENT = item['Date of Last Treatment'] && item['Date of Last Treatment'].length > 0
-      ? item['Date of Last Treatment'].length === 10
-        ? moment(item['Date of Last Treatment']).format('YYYY-MMMM-DD')
-        : item['Date of Last Treatment']
-      : '';
-    return [
-      ...modelInfo,
-      ...[
-        item['Pre/Post Collection'] || '',
-        item.Regime || '',
-        item.Treatment || '',
-        DATE_STARTED,
-        DATE_OF_LAST_TREATMENT,
-        item['Dose  (mg/day or mg/kg)'] || '', // TODO: Fix field name in the DB
-        Number.isFinite(item['Treatment Duration (Months)']) ? item['Treatment Duration (Months)'] : '',
-        item['Best Response (RECIST)'] || '',
-        Number.isFinite(item['Response Duration (Months)']) ? item['Response Duration (Months)'] : '',
-      ],
-    ];
-  });
+  const historyRows = history.map((item) => [
+    ...modelInfo,
+    ...[
+      item['Pre/Post Collection'] || '',
+      item.Regime || '',
+      item.Treatment || '',
+      item['Dose  (mg/day or mg/kg)'] || '', // TODO: Fix field name in the DB
+      Number.isFinite(item['Treatment Duration (Months)']) ? item['Treatment Duration (Months)'] : '',
+      item['Best Response (RECIST)'] || '',
+      Number.isFinite(item['Response Duration (Months)']) ? item['Response Duration (Months)'] : '',
+    ],
+  ]);
 
   return [...acc, ...historyRows];
 }, []);
@@ -73,7 +54,7 @@ module.exports.createWorksheet = (workbook, data = []) => {
     ...greyHeaderStyle,
   });
 
-  sheet.cell(3, 6, 3, 14).style({
+  sheet.cell(3, 6, 3, 12).style({
     ...alignmentStyle,
     ...blueHeaderStyle,
   });
