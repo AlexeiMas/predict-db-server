fe87fc86a8cdd058110a716bc31f5b7526cce101
albertbezman <albert.bezman@imagentherapeutics.com>
2022-01-12 23:29:46 +0000

    change export sheet formatting

--- services/export/sheets/clinical.service.js
+++ services/export/sheets/clinical.service.js
@@ -47,12 +47,12 @@ module.exports.createWorksheet = (workbook, data) => {
 
   setSheetBasicLayout(sheet);
 
-  sheet.cell(3, 3, 3, 6).style({
+  sheet.cell(4, 1, 4, 6).style({
     ...alignmentStyle,
     ...greyHeaderStyle,
   });
 
-  sheet.cell(3, 6, 3, 17).style({
+  sheet.cell(4, 4, 4, 17).style({
     ...alignmentStyle,
     ...blueHeaderStyle,
   });
